a05f3f5372eb68731f46443ba9e7941abc0b220d
Dac Minh Nhat  HO <minhnhat@MBP-de-Dac.viseo.net>
2019-09-23 16:55:23 +0200

    add ionicons

--- components/shop/ProductDetail.js
+++ components/shop/ProductDetail.js
@@ -1,16 +1,59 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import {color} from '../../constants/color';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const ProductDetail = () => {
+const ProductDetail = ({id, name, imageUrl, description, price}) => {
   return (
-    <View>
-      <Text>Product Detail</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{uri: imageUrl}} />
+      <View style={styles.details}>
+        <Text style={styles.price}>{price}</Text>
+        <Text style={styles.description}>{description}</Text>
+      </View>
+      <TouchableOpacity>
+        <Ionicons
+          style={styles.icon}
+          name="ios-cart"
+          size={48}
+          color={color.primaryColor}
+        />
+      </TouchableOpacity>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {},
+  image: {
+    height: 300,
+  },
+  details: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  price: {
+    color: color.thirdColor,
+    fontSize: 20,
+  },
+  description: {
+    color: color.primaryColor,
+    fontSize: 16,
+    fontStyle: 'italic',
+    textAlign: 'center',
+  },
+  icon: {
+    textAlign: 'center',
+  },
 });
 
 export default ProductDetail;
